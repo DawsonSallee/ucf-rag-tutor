05c224dcd80c59ce0183cdd9a9553e96f66ec14c
Dawson Sallee <dawsonsallee13@gmail.com>
2025-06-07 18:59:53 -0400

    fixed subject crash

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    if 'pysqlite3' in sys.modules:
+        sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
 import streamlit as st
 import os
